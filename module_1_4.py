6f136234baa28167e434390f9fe7eb8d1d111985
andreyalex2009 <andreyalex2009@yandex.ru>
2024-10-06 19:41:05 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+my_string = input("Какой сегодня день недели?: ")
+print(my_string)
+my_string = 'учимся языку программирования'
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
